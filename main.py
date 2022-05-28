105a37de20adf4709d71e572dd7bf2576561602f
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-05-28 16:19:53 +0300

    Зелёные человечки 1

---
+++ main.py
@@ -0,0 +1,27 @@
+from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
+import random
+
+def name(le):
+    ast = random.choice(ascii_lowercase + ascii_uppercase)
+    if le == 4:
+        a = 1
+        b = 1
+    else:
+        a = random.randrange(1, le-2)
+        b = le-a-3
+
+    c = False
+    while True:
+        m = random.choices(''.join(ascii_letters) + ''.join(digits), k=a)
+
+        for i in range(len(digits)):
+            if digits[i] in m:
+                c = True
+                break
+        if c:
+            break
+    ast += "".join(m)
+    ast += " "
+    ast += random.choice(ascii_uppercase[:len(ascii_uppercase) // 2])
+    ast += "".join(random.choices(ascii_lowercase, k=b))
+    return ast
