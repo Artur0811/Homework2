6bc6145580f516ce4305150d5feb96d072e97fac
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-06-08 17:15:58 +0300

    функция less_varietly

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
-import random
-
-def name(le):
-    ast = random.choice(ascii_lowercase + ascii_uppercase)
-    if le == 4:
-        a = 1
-        b = 1
-    else:
-        a = random.randrange(1, le-2)
-        b = le-a-3
-
-    while a > 0:
-        m = random.choice(''.join(ascii_letters) + ''.join(digits))
-        if m not in ast:
-            a-=1
-            ast+=m
-    ast += " "
-    while True:
-        a = random.choice(ascii_uppercase[:len(ascii_uppercase) // 2])
-        if a not in ast:
-            ast+=a
-            break
-    while b > 0:
-        m = random.choice(''.join(ascii_letters) + ''.join(digits))
-        if m not in ast:
-            b-=1
-            ast+=m
-    return ast
-
-def little_green_men_names(m,n):
-    rez = []
-    while len(rez)!= m:
-        a = name(n)
-        if a not in rez:
-            rez.append(a)
-    return rez
-
-print(*little_green_men_names(10, 12), sep="\n")
+from PIL import Image
+def less_varietly(image, name):
+    img = Image.open(image)
+    x, y = img.size[0]//2, img.size[1]//2
+    a = len(img.getcolors(maxcolors = 100000000))
+    while a > 256:
+        a = a//2
+    new = img.convert(mode = "P", colors = a).resize((x, y))
+    new = new.convert(mode = "RGB")
+    new.save(name)
+less_varietly(r"foto.jpg", r'new.jpg')
