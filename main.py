c60b380d333cb3d8ec245ed6a728a5a631d39186
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-06-14 13:31:06 +0300

    глаголы

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from pymorphy2 import MorphAnalyzer
-a  = "1"
-m = []
+a, m = "1", []
 while a != "":
     a = input()
     if a != "":
-        m.append(a.split(" "))
-    else:
-        break
-
+        m+=a.split()
+rez_s, rez_n = [], []
 for i in range(len(m)):
-    if MorphAnalyzer().parse(m[i][0])[0].tag.POS == "NOUN":
-        w = MorphAnalyzer().parse(m[i][1])[0]
-        s = MorphAnalyzer().parse(m[i][0])[0]
-        a = m[i][0]
-        m[i][0], m[i][1] = m[i][1], a
-    else:
-        s = MorphAnalyzer().parse(m[i][1])[0]
-        w = MorphAnalyzer().parse(m[i][0])[0]
-    w = w.inflect({s.tag.gender})
-    w = w.inflect({s.tag.case, s.tag.number})
-    print(w.word+ " "+ m[i][1])
-
+    if "," in m[i] or "." in m[i] or "!" in m[i]:
+        m[i] = m[i][:-1]
+    a = MorphAnalyzer().parse(m[i])[0]
+    if a.tag.POS == "VERB" or a.tag.POS == "INFN":
+        if "impf" in a.tag:
+            rez_n.append(a.normal_form)
+        else:
+            rez_s.append(a.normal_form)
+rez = sorted(list(set(rez_n))) + sorted(list(set(rez_s)))
+for i in rez:
+    print(i)
