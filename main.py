e32d808e9b921832b69405610db68350b063c799
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-07-05 21:29:06 +0300

    Fifo  and Lifo

--- main.py
+++ main.py
@@ -1,53 +1,45 @@
-from docx import Document
-from sys import stdin
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.shared import Pt, Inches
-def n2():
-    a = stdin.read().split("\n")
-    d = Document()
+class Fifo:
+    def __init__(self):
+        self.len = 0
+        self.items = []
+    def add_item(self, other):
+        self.len+=1
+        self.items.append(other)
+    def __getitem__(self, item):
+        return self.items[item]
+    def __len__(self):
+        return self.len
+    def empty(self):
+        self.items = []
+        self.len = 0
+    def empty_try(self):
+        return self.len == 0
 
-    h = d.add_paragraph()
-    h.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
-    h = h.add_run(a[0])
-    h.font.name = "Arial"
-    h.font.italic = True
-    h.font.size = Pt(11)
+class Lifo:
+    def __init__(self):
+        self.len = 0
+        self.items = []
+    def add_item(self, other):
+        self.len+=1
+        self.items=[other]+self.items
+    def __getitem__(self, item):
+        return self.items[item]
+    def __len__(self):
+        return self.len
+    def empty(self):
+        self.items = []
+        self.len = 0
+    def empty_try(self):
+        return self.len == 0
 
-    a = a[1:]
-    for i in range(len(a)-2):
-        t = d.add_paragraph()
-        t.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
-        t.paragraph_format.first_line_indent = Inches(0.5)
-        t = t.add_run(a[i])
-        t.font.name = "Times New Roman"
-        t.font.size = Pt(12)
-
-    e1 = d.add_paragraph()
-    e1.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    e1 = e1.add_run(a[-2])
-    e2 = d.add_paragraph()
-    e2.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    e2 = e2.add_run(a[-1])
-    e2.font.bold = True
-
-    d.save("letter.docx")
-
-def n1():
-    a = stdin.read().split()
-    d = Document()
-    h = d.add_heading("Blood test", level=0)
-    h.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-    t = d.add_table(len(a) + 1, 3, style="Table Grid")
-    head_t = t.rows[0].cells
-    head = ["ingicator", "norm", "value"]
-    for i in range(3):
-        head_t[i].text = head[i]
-        r = head_t[i].paragraphs[0].runs[0]
-        r.font.bold = True
-        head_t[i].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-    for i in range(1, len(a) + 1):
-        s = t.rows[i].cells
-        s[0].text = a[i - 1]
-    d.save("analysis.docx")
-
-n1()
+fi = Fifo()
+li =Lifo()
+for i in range(10):
+    fi.add_item(i)
+    li.add_item(i)
+print(*fi, "|", *li, sep=" ")
+print(len(li), len(fi))
+print(li[8], fi[8])
+print(li.empty_try())
+li.empty()
+print(li.empty_try())
