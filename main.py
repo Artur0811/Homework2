f6974d6ffa919c8ac9c1fe5e1cd98044189e0209
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-07-07 14:50:28 +0300

    Fifo and Lifo

--- main.py
+++ main.py
@@ -1,45 +1,75 @@
+class obj:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
 class Fifo:
     def __init__(self):
+        self.head = None
         self.len = 0
-        self.items = []
-    def add_item(self, other):
+    def add_el(self, item):
         self.len+=1
-        self.items.append(other)
-    def __getitem__(self, item):
-        return self.items[item]
-    def __len__(self):
+        if self.head == None:
+            self.head = obj(item)
+        else:
+            k = self.head
+            while k.next != None:
+                k=k.next
+            k.next = obj(item)
+        return
+    def get_el(self):
+        if self.head != None:
+            return self.head.item
+        else:
+            return None
+    def size(self):
         return self.len
-    def empty(self):
-        self.items = []
-        self.len = 0
     def empty_try(self):
-        return self.len == 0
+        return self.head == None
+    def empty(self):
+        self.head =None
 
 class Lifo:
     def __init__(self):
+        self.head = None
         self.len = 0
-        self.items = []
-    def add_item(self, other):
+    def add_el(self, item):
         self.len+=1
-        self.items=[other]+self.items
-    def __getitem__(self, item):
-        return self.items[item]
-    def __len__(self):
+        if self.head == None:
+            self.head = obj(item)
+        else:
+            k = obj(item)
+            k.next = self.head
+            self.head = k
+        return
+    def get_el(self):
+        if self.head == None:
+            return None
+        else:
+            return self.head.item
+    def size(self):
         return self.len
-    def empty(self):
-        self.items = []
-        self.len = 0
     def empty_try(self):
-        return self.len == 0
+        return self.head == None
+    def empty(self):
+        self.head =None
 
-fi = Fifo()
-li =Lifo()
-for i in range(10):
-    fi.add_item(i)
-    li.add_item(i)
-print(*fi, "|", *li, sep=" ")
-print(len(li), len(fi))
-print(li[8], fi[8])
-print(li.empty_try())
-li.empty()
-print(li.empty_try())
+f = Fifo()
+print(f.empty_try())
+for i in range(1, 10):
+    f.add_el(i)
+print(f.get_el())
+print(f.size())
+print(f.empty_try())
+f.empty()
+print(f.empty_try())
+print("----------------------------")
+l = Lifo()
+print(l.empty_try())
+for i in range(1, 10):
+    l.add_el(i)
+print(l.get_el())
+print(l.size())
+print(l.empty_try())
+l.empty()
+print(l.empty_try())
