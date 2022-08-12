1284422db17b13fa21d69c9928fd65fcd685ccfe
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-08-12 17:01:27 +0300

    №26

--- main.py
+++ main.py
@@ -1,79 +1,31 @@
-def Pmatrix(x, y):
-  m = [[0 for i in range(len(x))] for i in range(len(x))]
-  for i in range(len(x)):
-    for z in range(len(y)):
-      for h in range(len(y)):
-        m[i][z] += x[i][h]*y[h][z]
-  return m
-
-def Smatrix(x, y):
-  for i in range(len(x)):
-    for z in range(len(y)):
-      x[i][z]+=y[i][z]
-  return x
-
-def d(x):
-  m1, m2, m3, m4, l = [], [], [], [], len(x[0])
-  for i in range(l):
-    m2.append(x[i][l // 2:])
-    m1.append(x[i][:l // 2])
-  m3 = m1[len(m1) // 2:]
-  m1 = m1[:len(m1) // 2]
-  m4 = m2[len(m2) // 2:]
-  m2 = m2[:len(m2) // 2]
-  return [m1, m2, m3, m4]
-
-def rek(x ,y):
-  if len(x) > 2:
-    x = d(x)
-    y = d(y)
-    return skleyka([Smatrix(rek(x[0], y[0]), rek(x[1], y[2])),  Smatrix(rek(x[0], y[1]), rek(x[1], y[3])),  Smatrix(rek(x[2], y[0]), rek(x[3], y[2])),Smatrix(rek(x[2], y[1]), rek(x[3], y[3]))])
-  else:
-    return Pmatrix(x, y)
-
-def skleyka(x):
-  m = []
-  for i in range(len(x[0])):
-    m.append(x[0][i] + x[1][i])
-  for i in range(len(x[0])):
-    m.append(x[2][i] + x[3][i])
-  return m
-
-k = [[1, 2, 3, 4],
-     [5, 2, 6, 4],
-     [1, 7, 9, 8],
-     [0, 12, 4, 4]]
-b = [[3, 5, 3, 7],
-     [8, 112, 23, 4],
-     [1, 34, 9, 567],
-     [0, 12, 0, 4]]
-
-
-k = [[1, 0],
-     [0,1]]
-b = [[1,1],
-     [3, 5]]
-
-
-k = [[2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1]]
-b = [[2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [2, 2, 2, 2, 0, 0, 0, 0],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1],
-      [0, 0, 0, 1, 1, 1, 1, 1]]
-r = rek(k,b)
-for i in r:
-  print(i)
-p = Pmatrix(k,b)
-for i in p:
-  print(i)
+with open ("26 (1).txt") as f :
+    n = f.readline()
+    a = []
+    for i in f:
+        a.append(list(map(int, i.split())))
+r = []
+s = 0
+for i in range(len(a)):
+    if a[i][0]<=1634515200 and a[i][1]>1634515200 or a[i][0]<=1634515200 and a[i][1]==0:
+        s+=1
+    if 1634515200 < a[i][0]< 1634515200+7*24*3600:
+        r.append([a[i][0], 0])
+    if 1634515200 < a[i][1]< 1634515200+7*24*3600:
+        r.append([a[i][1], 1])
+r=sorted(r, key=lambda x:x[0])
+sm = s
+t = 0
+for i in range(len(r)):
+    if r[i][1] == 0:
+        s+=1
+        tn = r[i][0]
+    else:
+        s-=1
+    if sm < s:
+        sm = s
+        if i+1 != len(r):
+            if r[i+1][1] == 1:
+                t= r[i+1][0] - tn
+        else:
+            t = 1634515200+7*24*3600-tn
+print(sm, t)
