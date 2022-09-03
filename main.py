5921d43675d5cf62f382b959f0abc91681f95ce5
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-09-03 13:14:40 +0300

    свои ошибки

--- main.py
+++ main.py
@@ -1,31 +1,73 @@
-def schet(vvod):
-    def sum(a, b):
-      return a[1]+b[1]
+import random
+import re
+class CardError(Exception):
+    pass
 
-    def delenie(m, k = 0):
-      if len(m)>1:
-        a = delenie(m[:len(m)//2], k)
-        b = delenie(m[len(m)//2:], k)
-        k+=a[1]
-        k+=b[1]
-        c = sliyanie(a[0], b[0], k)
-        return c[0], c[1]
-      else:
-        return m, k
+class CardFormatError(CardError):
+    pass
+
+class CardAlgorithmError(CardError):
+    pass
+
+class words_in_number(CardError):
+  pass
+
+class low_len(CardError):
+  pass
+
+class err_server(CardError):
+  pass
+
+class empty_input(CardError):
+  pass
 
 
-    def sliyanie(a, b, k = 0):
-      m = []
-      while len(a)> 0 and len(b)>0:
-        if a[0] > b[0]:
-          k+= len(a)
-          m.append(b[0])
-          b = b[1:]
-        else:
-          m.append(a[0])
-          a = a[1:]
-      m+=a
-      m+=b
-      return m, k
-    return delenie(vvod)
-print(schet([9,8, 1,2,3,4]))
+
+def get_card_number():
+    card_number = input("Введите номер карты (16 цифр): ")
+    card_number = "".join(card_number.strip().split())
+    if card_number.isdigit() and len(card_number) == 16:
+        return card_number
+    else:
+      if card_number =="":
+        raise empty_input("Пустой ввод")
+      elif card_number.isalnum():
+        raise words_in_number("Буквы в номере")
+      elif len(card_number)<16:
+        raise low_len("Символов меньше 16")
+      raise CardFormatError('Неверный формат номера')
+
+
+def double(x):
+    res = x * 2
+    if res > 9:
+        res = res - 9
+    return res
+
+
+def luhn_algorithm(card):
+    odd = map(lambda x: double(int(x)), card[::2])
+    even = map(int, card[1::2])
+    if (sum(odd) + sum(even)) % 10 == 0:
+        return True
+    else:
+      raise CardAlgorithmError('Недействительный номер карты')
+
+
+def process():
+    while True:
+
+        try:
+            number = get_card_number()
+            if luhn_algorithm(number):
+              if random.randint(1, 6) == 5:
+                raise err_server("Ошибка сервера")
+              print("Ваша карта обрабатывается...")
+              break
+            else:
+                print("Номер недействителен. Попробуйте снова.")
+        except CardError as e:
+            print('Ошибка!', e)
+
+
+process()
