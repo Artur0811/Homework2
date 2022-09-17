f7d5e74bce886160da239deee983e9b9d1b59700
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-09-17 18:56:57 +0300

    27 теперь точно верно

--- main.py
+++ main.py
@@ -1,61 +1,3 @@
-def n23():
-    def f(x, y):
-        if x == y:
-            return 1
-        if x > y:
-            return 0
-        if x == 12:
-            return 0
-        return f(x+1, y)+f(x+2, y)+f(x*3, y)
-    print(f(2, 9)*f(9, 19))#650
-
-def n24():
-    with open("24_demo (1).txt") as f:
-        a = f.read()
-        k = 0
-        ma = 0
-        for i in range(len(a)):
-            if a[i] == "Y":
-                k+=1
-            else:
-                if ma<k:
-                    ma = k
-                k = 0
-        print(ma)#10
-
-def n25():
-    for i in range(201455, 201471):
-        m = []
-        for y in range(1, round(i**0.5)+1):
-            if i%y == 0:
-                if i//y == y:
-                    m.append(y)
-                else:
-                    m.append(y)
-                    m.append(i//y)
-            if len(m) > 4:
-                break
-        if len(m) == 4:
-            print(sorted(m))
-            #[1, 3, 67153, 201459]
-            #[1, 13, 15497, 201461]
-            #[1, 29, 6947, 201463]
-            #[1, 2, 100733, 201466]
-
-def n26():
-    with open("27886.txt") as f:
-        s, n = map(int, f.readline().split())
-        a = sorted(map(int, f))
-        k, ma = 0, 0
-        for i in range(len(a)):
-            if s-a[i] > 0:
-                k+=1
-                s-=a[i]
-                ma = a[i]
-    ma+=s
-    while ma not in a:
-        ma -=1
-    print(k, ma)#458 39
 def n27(name):
     with open(name) as f:
         n = f.readline()
@@ -69,7 +11,7 @@ def n27(name):
             if a[i]%2 == 1:
                 left.append(k)
                 k = 0
-                if  len(left) == l:
+                if len(left) == l:
                     break
         right = []
         for i in range(1, len(a)):
@@ -79,12 +21,11 @@ def n27(name):
                 k = 0
                 if len(right) == l:
                     break
-        for i in range(l):
-            if left[i] > right[i]:
-                summa -=right[i]
-            else:
-                summa-=left[i]
-        print(summa)
+        msx_sum = 0
+        for i in range(l+1):
+            if summa-sum(right[i:])-sum(left[:i])>msx_sum:
+                msx_sum = summa-sum(right[i:])-sum(left[:i])
 
+        print(msx_sum)
 n27("27-A.txt")#4777208
 n27("27-B.txt")#979268310
