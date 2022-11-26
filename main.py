69f431e398bab86b8e8d24188ec20fa09a8c4edd
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2022-11-26 17:22:44 +0300

    джерримендеринг

--- main.py
+++ main.py
@@ -15,13 +15,44 @@ def number():
 
 n = int(input())
 m = [int(input()) for i in range(n)]
-t = False
+p = [0]*n
+p[0] = m[0]
+mp = []
 for i in range(1, n):
-    for y in range(i+1, n):
-        a= sorted([sum(m[:i]), sum(m[i:y]), sum(m[y:])])
-        if a[1]>0 and a[2] > 0:
-            print(i, y-i, n-y)
-            t = True
-            break
-    if t:
+    if p[i-1] > 0 and m[i] > 0:
+        p[i] = m[i]
+        mp.append(i)
+    elif m[i]<= 0 and p[i-1] + m[i] > 0:
+        p[i] = m[i] + p[i-1]
+    elif p[i-1] > 0 and p[i-1] + m[i]<= 0:
+        p[i] = m[i]
+        mp.append(i)
+    else:
+        p[i] = m[i] + p[i - 1]
+    if len(mp) ==2:
         break
+ml =[]
+l = [0]*n
+l[-1] = m[-1]
+for i in range(-2, -n-1, -1):
+    if l[i+1]+m[i]>0 and m[i]<0:
+        l[i] = m[i]+l[i+1]
+    elif l[i+1]>0 and l[i+1]+m[i]<=0:
+        l[i] = m[i]
+        ml=[n+i+1]+ml
+    elif l[i+1]>0 and m[i] > 0:
+        l[i] = m[i]
+        ml=[n+i+1]+ml
+    else:
+        l[i] = m[i] + l[i + 1]
+    if len(ml) ==2:
+        break
+if len(mp) == 2:
+    print(mp[0], mp[1]-mp[0], n- mp[1])
+elif len(mp) == 1 and len(ml) == 1:
+    if mp[0] < ml[0]:
+        print(mp[0], ml[0]-mp[0], n - ml[0])
+elif len(ml) == 2:
+    print(ml[0], ml[1] - ml[0], n-ml[1])
+else:
+    print(0)
