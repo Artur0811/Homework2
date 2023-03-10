729e36ff242c8a8369705ce893a35445137a1fe7
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2023-03-10 16:36:49 +0300

    дз

--- main.py
+++ main.py
@@ -1,35 +1,71 @@
-def f(stroka, patern, f_st = "", n= 0, k = 0):
-    if len(patern) == k:
-        return stroka == f_st
-    if patern[k] == ".":
-        if len(stroka) == n:
-            return False
-        return f(stroka, patern, f_st+stroka[n], n+1, k+1)
-    if len(patern) -1 > k:
-        if patern[k+1] == "*":
-            if n != len(stroka):
-                if stroka[n] == patern[k]:
-                    return f(stroka, patern, f_st+ stroka[n], n+1, k)
-            return f(stroka, patern, f_st, n, k+2)
-    return f(stroka, patern, f_st + patern[k], n+1,k+1)
+s = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+def f(num, s, per = ""):
+    zn = {}
+    zn[0] = ""
+    raz = 1
+    for i in range(len(s)):
+        if i%2 == 0:
+            for y in range(1, 4):
+                zn[raz*y] = s[i]*y
+            if i != len(s)-1:
+                zn[4*raz] = s[i]+s[i+1]
+            if i!= 0:
+                zn[9*raz//10] = s[i-2]+s[i]
+        else:
+            for y in range(4):
+                zn[raz*(5+y)] = s[i]+ s[i-1]*y
+            raz = raz*10
+    raz = 10**(len(str(num))-1)
+    while raz > 0:
+        if raz == 1:
+            per += zn[num%10]
+        else:
+            per += zn[num//raz*raz]
+        num = num - num // raz * raz
+        raz = raz//10
+    return per
 
-print(f("aaaaaaasdfsg", "b*a*....."))
+for i in range(2900, 3000):
+    print(f(i, s), i)
 
-
-def func(data, p):
-    data = sorted(data)
-    mi = 99999999
-    s = 999999999
-    for i in range(len(data)):
-        for y in range(i+2, len(data)):
+rez = []
+def f1(m, p):
+    m = sorted(m)
+    len_m = len(m)
+    s = {}
+    for i in m:
+        s[i] = m.count(i)
+    for i in range(len_m):
+        for y in range(i+2, len_m):
             k = i+1
             while k < y:
-                if abs(p - data[i] - data[y] - data[k]) < mi:
-                    mi = abs(p - data[i] - data[y] - data[k])
-                    s = data[i] + data[y] + data[k]
-                k+=1
-    return s
-#
-# print(func([-1, 2, 1, -4], 1))
-for i in range(-10, 11):
-    print(func([-5, -10,  2, 1, -7], i), i)
+                if p - m[i] - m[y] - m[k] in s:
+                    el = p - m[i] - m[y] - m[k]
+                    zn = sorted([m[i], m[k], m[y],el])
+                    if zn not in rez and zn.count(el)<= s[el]:
+                        rez.append(zn)
+                k += 1
+f1([1,0,-1,0,-2,2], 0)
+for i in rez:
+  print(i)
+
+def moda(a, b):
+    c = []
+    k =0
+    n = 0
+    while k < len(a) and n < len(b):
+        if a[k] > b[n]:
+            c.append(b[n])
+            n+=1
+        else:
+            c.append(a[k])
+            k+=1
+    c+=a[k:]
+    c+=b[n:]
+    print(c)
+    if len(c)%2 == 1:
+        return c[len(c)//2]
+    else:
+        return (c[len(c)//2] + c[len(c)//2-1])/2
+
+print(moda([1, 3, 6, 9, 19], [-4, -2, 0, 28, 99]))
