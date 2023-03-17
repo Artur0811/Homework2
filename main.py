5b71e92ea3e134601a278f885a99b61fa3a0ff65
Artur0811 <105109279+Artur0811@users.noreply.github.com>
2023-03-17 18:39:16 +0300

    дз

--- main.py
+++ main.py
@@ -1,71 +1,72 @@
-s = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-def f(num, s, per = ""):
-    zn = {}
-    zn[0] = ""
-    raz = 1
-    for i in range(len(s)):
-        if i%2 == 0:
-            for y in range(1, 4):
-                zn[raz*y] = s[i]*y
-            if i != len(s)-1:
-                zn[4*raz] = s[i]+s[i+1]
-            if i!= 0:
-                zn[9*raz//10] = s[i-2]+s[i]
-        else:
-            for y in range(4):
-                zn[raz*(5+y)] = s[i]+ s[i-1]*y
-            raz = raz*10
-    raz = 10**(len(str(num))-1)
-    while raz > 0:
-        if raz == 1:
-            per += zn[num%10]
-        else:
-            per += zn[num//raz*raz]
-        num = num - num // raz * raz
-        raz = raz//10
-    return per
+def n24_1():
+    with open("f.txt") as f:
+        a = f.read().strip()
+        s = {}
+        for i in range(len(a)-1):
+            if a[i] == "E":
+                if a[i+1] in s:
+                    s[a[i+1]] +=1
+                else:
+                    s[a[i+1]] = 1
+        m = 0
+        b = ""
+        for i in s:
+                if s[i] > m:
+                    m = s[i]
+                    b = i
+    print(b)#Y
 
-for i in range(2900, 3000):
-    print(f(i, s), i)
+def n24_2():
+    with open("f.txt") as f:
+        a = f.read().strip()
+        e = 0
+        l = 0
+        ma = 0
+        for i in range(len(a)):
+            if a[i] == "E":
+                e+=1
+                l+=1
+            elif a[i] == "A":
+                if e >=3:
+                    ma = max(ma, l)
+                e = 0
+                l = 0
+            else:
+                l+=1
+        print(ma)#282
 
-rez = []
-def f1(m, p):
-    m = sorted(m)
-    len_m = len(m)
-    s = {}
-    for i in m:
-        s[i] = m.count(i)
-    for i in range(len_m):
-        for y in range(i+2, len_m):
-            k = i+1
-            while k < y:
-                if p - m[i] - m[y] - m[k] in s:
-                    el = p - m[i] - m[y] - m[k]
-                    zn = sorted([m[i], m[k], m[y],el])
-                    if zn not in rez and zn.count(el)<= s[el]:
-                        rez.append(zn)
-                k += 1
-f1([1,0,-1,0,-2,2], 0)
-for i in rez:
-  print(i)
+def n25_1():
+    n1 = [str(i) for i in range(10)]
+    p1 = [""] +[str(i) for i in range(1000)]
+    rez = []
+    for i in range(len(n1)):
+        for y in range(len(p1)):
+            num = int("1"+n1[i]+"2139"+p1[y]+"4")
+            if num%2023 == 0:
+                rez.append([num, num//2023])
+    rez = sorted(rez, key=lambda x: x[0])
+    for i in rez:
+        print(*i)
+        #162139404 80148
+        #1321399324 653188
+        #1421396214 702618
+        #1521393104 752048
 
-def moda(a, b):
-    c = []
-    k =0
-    n = 0
-    while k < len(a) and n < len(b):
-        if a[k] > b[n]:
-            c.append(b[n])
-            n+=1
-        else:
-            c.append(a[k])
-            k+=1
-    c+=a[k:]
-    c+=b[n:]
-    print(c)
-    if len(c)%2 == 1:
-        return c[len(c)//2]
-    else:
-        return (c[len(c)//2] + c[len(c)//2-1])/2
-
-print(moda([1, 3, 6, 9, 19], [-4, -2, 0, 28, 99]))
+def n25_2():
+    def f(n):
+        k = 0
+        for i in range(1, round(n**0.5)+1):
+            if n%i == 0:
+                if i%2 == 1:
+                    k+=1
+                if (n//i)%2 == 1:
+                    k+=1
+            if k > 5:
+                break
+        return k == 5
+    for i in range(35000000, 40000001):
+        if f(i):
+            print(i)
+            #35819648
+            #39037448
+            #39337984
